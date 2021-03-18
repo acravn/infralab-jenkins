fe06451b231048ae9816287553d3bb93d6f7c054
Andrew Ravn <ravn.andrew@gmail.com>
2021-03-18 14:29:08 -0600

    testing aws command

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,11 +1,13 @@
 #!/usr/bin/env groovy
+import groovy.json.JsonSlurper
 
 def helloworld() {
-    def list = sh(script: 'aws ec2 describe-volumes --region us-east-1 --query \'Volumes[*].VolumeId\'', returnStdout: true)
+    def ids = sh(script: 'aws ec2 describe-volumes --region us-east-1 --query \'Volumes[*].VolumeId\'', returnStdout: true)
 //    echo "Here's are list: ${list}"
+    def list = new JsonSlurper().parseText(ids)
 
-    for(i in list) {
-        echo i
+    for(item in list) {
+        println(item)
     }
 }
 

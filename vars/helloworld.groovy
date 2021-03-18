9ba2bd4b462d9ade35bbb672d7cfb7ed368f16a0
Andrew Ravn <ravn.andrew@gmail.com>
2021-03-18 14:23:27 -0600

    testing aws command

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def helloworld() {
-    def list = sh('aws ec2 describe-volumes --region us-east-1 --query \'Volumes[*].VolumeId\'')
+    def list = sh(script: 'aws ec2 describe-volumes --region us-east-1 --query \'Volumes[*].VolumeId\'', returnStdout: true)
 
     echo list
 }
